e34e789b5ae59d380946c10f868de038df4b425c
Omal Harsha <omalharshanuwan@gmail.com>
2023-09-27 08:27:26 +0530

    basic CRUD

--- index.js
+++ index.js
@@ -1,8 +1,70 @@
 const express = require("express");
 
 const app = express()
+app.use(express.json())
+const mysql = require("mysql2")
+
+const db = require("./models")
+const {User} = require('./models')
+
+app.get('/select-all', (req,res) => {
+    User.findAll()
+    .then((users) => {
+        res.send(users)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+app.get('/select-one', (req,res) => {
+
+    const {id} = req.query
+
+    User.findOne({where: {id:id}})
+    .then((users) => {
+        res.send(users)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+app.post('/insert', (req,res) => {
+
+    const {firstName, age} = req.body
+    User.create({
+        firstName, 
+        age
+    }).then((user) => {
+        res.status(201).json({ message: "User created successfully", user });
+    }).catch((err) => {
+        if(err){
+            console.log(err)
+        }
+    })
+    console.log("user insert")
+})
+
+app.delete('/delete', (req,res) => {
+    const {id} = req.query
+    User.destroy({where: {id:id}})
+    .then((result)=> {
+        res.status(201).json({ message: "User deleted successfully", result });
+    })
+    .catch((err) => {
+        if(err){
+            console.log(err)
+        }
+    })
+    
+})
 
 
-app.listen(3001, ()=> {
-    console.log("server running")
+
+db.sequelize.sync().then((req) => {
+
+    app.listen(3001, ()=> {
+        console.log("server running")
+    })
 })
